b5e0f9d6e5a9a05deb7e4c41124376c559e27bd9
Thomas <tlloevkilde@gmail.com>
2024-10-25 12:44:07 +0200

    Add project files.

---
+++ UmbracoCommunity/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoCommunity.ViewModels;
+
+namespace UmbracoCommunity.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        public ContactFormSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public IActionResult Submit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            TempData["success"] = true;
+            var cs = Services.ContentService;
+            if(cs != null)
+            {
+                IContent comment = cs.Create(model.Subject, CurrentPage.Id, "Comment");
+                comment.SetValue("username", model.Name);
+                comment.SetValue("email", model.Email);
+                comment.SetValue("subject", model.Subject);
+                comment.SetValue("message", model.Message);
+                cs.Save(comment);
+            }
+
+            //Succes - so go back to current page
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ UmbracoCommunity/ViewModels/ContactForm .cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoCommunity.ViewModels
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress(ErrorMessage = "not valid email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Subject { get; set; } = string.Empty;
+
+        [Required]
+        public string Message { get; set; } = string.Empty;
+    }
+}
